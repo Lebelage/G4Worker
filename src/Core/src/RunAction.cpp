f8bb3ddb0724b286f235127b13805a6a9583c866
Tim <exceptioncast@outlook.com>
2026-02-18 14:57:43 +0300

    Bugs fixed

--- src/Core/src/RunAction.cpp
+++ src/Core/src/RunAction.cpp
@@ -11,7 +11,7 @@ G4Worker::RunAction::RunAction()
     ana->SetNtupleMerging(true);
 
     // depth: 0..3.1 um
-    const G4int nbins = 310; // 10 nm на бин
+    const G4int nbins = 3000; // 10 nm на бин
     const G4double dmin = 0.0;
     const G4double dmax = 3.1 * um;
     ana->CreateH1("Edep_vs_depth", "Edep vs depth; depth [um]; Edep [MeV]",
